1432c997f8ac8564eee4ad3e0bce49447735de67
Анастасия <anastasiatomonova54@.gmail.com>
2026-03-25 21:57:30 +0300

    Считали и вывели содержимое файла

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
-$fd = fopen("test.txt", 'w+') or die("не удалось открыть файл");
+$fd = fopen("test.txt", 'w') or die("не удалось открыть файл");
 
 $str = "Привет мир";
 fwrite($fd, $str);
 
-$fd = fopen("test.txt", 'r+') or die("не удалось открыть файл");
+$fd = fopen("test.txt", 'r') or die("не удалось открыть файл");
 fclose($fd);
+
+
+$fd = fopen("test.txt", 'r') or die("Не удалось открыть файл для чтения");
+
+$content = fread($fd, filesize("test.txt"));
+
+fclose($fd);
+
+echo "Содержимое файла: <strong>$content</strong><br><br>";
 
 
 ?>
