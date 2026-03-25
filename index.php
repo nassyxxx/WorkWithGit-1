f08c757dd1757cba8a2b4ac74631d552d653ea6c
Анастасия <anastasiatomonova54@.gmail.com>
2026-03-25 22:02:09 +0300

    Переименовали файл в mir.txt

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+#задание №1
 $fd = fopen("test.txt", 'w') or die("не удалось открыть файл");
 
 $str = "Привет мир";
@@ -7,7 +8,7 @@ fwrite($fd, $str);
 $fd = fopen("test.txt", 'r') or die("не удалось открыть файл");
 fclose($fd);
 
-
+#задание №2
 $fd = fopen("test.txt", 'r') or die("Не удалось открыть файл для чтения");
 
 $content = fread($fd, filesize("test.txt"));
@@ -15,6 +16,15 @@ $content = fread($fd, filesize("test.txt"));
 fclose($fd);
 
 echo "Содержимое файла: <strong>$content</strong><br><br>";
+
+#задание №3
+
+if (rename("test.txt", "mir.txt")) {
+    echo "Файл переименован: <br>";
+} else {
+    echo "Не удалось переименовать файл";
+}
+
 
 
 ?>
