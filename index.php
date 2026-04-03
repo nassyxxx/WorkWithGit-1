b2ddcafe748c31f8ae4bf57537fc67ad4ec8a31c
Анастасия <anastasiatomonova54@.gmail.com>
2026-04-03 03:07:53 +0300

    Изменили метод getAge на setAge и седали сво-во age приватным.Также для получения возраста сделали новый метод showAge

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
 class Rabotnik {
     public $name;
-    public $age;
+    private $age;
     public $salary;
 
     public function getName() {
         return $this->name;
     }
 
-   public function getAge() {
+    public function setAge($newAge) {
+        $this->age = $newAge;
+    }
+
+   public function showAge() {
         return $this->age;
     }
+
+
 
        public function getSalary(...$otherWorkers) {
         $total = $this->salary;
@@ -27,21 +33,21 @@ $worker1 = new Rabotnik();
 $worker2 = new Rabotnik();
 
 $worker1->name = "Иван";
-$worker1->age = 25;
+$worker1->setAge(25);
 $worker1->salary = 50000;
 
 $worker2->name = "Ольга";
-$worker2->age = 30;
+$worker2->setAge(30);
 $worker2->salary = 60000;
 
 $sumSalary = $worker1->salary + $worker2->salary;
-$sumAge = $worker1->age + $worker2->age;
+$sumAge = $worker1->showAge() + $worker2->showAge();
 
 echo "Сумма зарплат: " . $sumSalary . "<br>";
 echo "Сумма возрастов: " . $sumAge . "<br>";
 
 echo "Имя работника 1: " . $worker1->getName() . "<br>";
-echo "Возраст работника 1: " . $worker1->getAge() . "<br>";
+echo "Возраст работника 1: " . $worker1->showAge() . "<br>";
 echo "Зарплата работника 1: " . $worker1->getSalary() . "<br>";
 
 echo "Общая сумма зарплат (через getSalary): " . $worker1->getSalary($worker2) . "<br>";
